211386a7c5040b0251acd738e07d01c53db98082
Sefi <jselbaz@gmail.com>
2021-09-23 16:22:47 -0500

    Finished

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
 const grid = document.querySelector('.grid');
 let gridValue = document.querySelector('.grid-size');
-let gridSize = document.querySelector('input');
+let gridSize = document.querySelector('.size-slider');
+const colorBtn = document.querySelector('.colorBtn');
+const eraseBtn = document.querySelector('.eraseBtn');
 const clearBtn = document.querySelector('.clearBtn');
 const adjustGridSize = document.querySelector('.apply');
 let size = 16;
-
+let erase = false;
+let currentColor = '#DCDCDC';
 makeGrid(size);
 
 function makeCell(size) {
@@ -28,21 +31,52 @@ function changeSize() {
     grid.removeChild(grid.lastChild);
   }
   makeGrid(size);
-}
+}  
+
+colorBtn.addEventListener('click', function() {
+  erase = false;
+})
+
+eraseBtn.addEventListener('click', function() {
+  erase = true;
+})
 
 grid.addEventListener('mouseover', function(e) {
-  if (e.target.matches('.cell')) {
+  if (e.target.matches('.cell') && erase === false) {
+    e.target.classList.remove('erased');
     e.target.classList.add('active');
   }
-});
+})
+
+grid.addEventListener('mouseover', function(e) {
+  if (e.target.matches('.cell') && erase === true) {
+    e.target.classList.remove('active');
+    e.target.classList.add('erased');
+  }
+})
 
 gridSize.addEventListener('input', function(e) {
   size = e.target.value;
   gridValue.textContent = `${size}x${size}`;
-});
+})
 
 adjustGridSize.addEventListener('click', function() {
   changeSize();
-});
+})
 
 adjustGridSize.addEventListener('click', changeSize);
+
+clearBtn.addEventListener('click', function() {
+  changeSize(size);
+})
+
+/*
+Potential future changes to change coloring mode color
+let colorPicker = document.querySelector('.colorPicker');
+
+function setCurrentColor(color) {
+  currentColor = color;
+}
+
+colorPicker.onchange = (e) => setCurrentColor(e.target.value)
+*/
